e2800d8456f808bcfd37930a492359a6d1abcb73
mamoon100 <mamoon.husen1006@gmail.com>
2025-11-04 18:54:13 +0300

    add monitoring via Micrometer and Spring Actuator, update CaptchaController to track metrics, and configure Prometheus registry.

--- src/main/java/com/mealplanet/captcha/controller/CaptchaController.java
+++ src/main/java/com/mealplanet/captcha/controller/CaptchaController.java
@@ -5,6 +5,7 @@ import com.mealplanet.captcha.model.request.CaptchaValidationRequest;
 import com.mealplanet.captcha.model.response.CaptchaResponse;
 import com.mealplanet.captcha.model.response.CaptchaValidationResponse;
 import com.mealplanet.captcha.service.CaptchaService;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +17,18 @@ public class CaptchaController {
 
 
     private final CaptchaService captchaService;
+    private final MeterRegistry meterRegistry;
 
-    public CaptchaController(CaptchaService captchaService) {
+    public CaptchaController(CaptchaService captchaService, MeterRegistry meterRegistry) {
         this.captchaService = captchaService;
+        this.meterRegistry = meterRegistry;
     }
 
     @PostMapping("/generate")
     public CaptchaResponse generateCaptcha(@RequestParam CaptchaTypeEnum type) {
-        return captchaService.generateCaptcha(type);
+        CaptchaResponse captchaResponse = captchaService.generateCaptcha(type);
+        meterRegistry.counter("captcha.generated.count").increment();
+        return captchaResponse;
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.IMAGE_PNG_VALUE)
@@ -33,7 +38,14 @@ public class CaptchaController {
 
     @PostMapping("/validate/{id}")
     public CaptchaValidationResponse validateCaptcha(@PathVariable UUID id, @RequestBody CaptchaValidationRequest captcha) {
-        return captchaService.validateCaptcha(id, captcha);
+        meterRegistry.counter("captcha.verification.count").increment();
+        CaptchaValidationResponse captchaValidationResponse = captchaService.validateCaptcha(id, captcha);
+        if (captchaValidationResponse.valid()) {
+            meterRegistry.counter("captcha.valid.count").increment();
+        } else {
+            meterRegistry.counter("captcha.invalid.count").increment();
+        }
+        return captchaValidationResponse;
     }
 
 
